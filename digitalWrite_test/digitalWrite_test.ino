30720c07a348d83779ab4a702e4837f2f602ca06
JArgillander <joakim.argillander@telia.com>
2013-09-20 16:30:41 +0200

    Test

---
+++ digitalWrite_test/digitalWrite_test.ino
@@ -0,0 +1,42 @@
+
+
+
+int ledgreen = 2;
+int ledyellow = 3; 
+int ledred = 4;
+int buttonstate = 0;
+
+const int knapp1 = 7;
+const int knapp2 = 8;
+
+
+void setup() {
+  pinMode(ledgreen, OUTPUT); 
+  pinMode(ledyellow, OUTPUT);
+  pinMode(ledred, OUTPUT);
+  pinMode(knapp1, INPUT);
+  pinMode(knapp2, INPUT); 
+}
+
+void check(){
+  if (buttonstate == HIGH) {
+  
+    digitalWrite(ledred, HIGH);
+    delay(50);
+    digitalWrite(ledred, LOW);
+    delay(50);
+    
+  }
+}  
+   
+void loop(){
+  buttonstate = digitalRead(knapp1);
+ 
+check();
+  
+ digitalWrite(ledgreen, HIGH);
+ delay(100); 
+ digitalWrite(ledgreen, LOW);
+ delay(100); 
+ 
+}
